f59a5323824059990bfdda2c80f26d8458618cd7
luoguoshun <394024317@qq.com>
2023-03-20 10:10:14 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from '@vue/cli-service';
-export default defineConfig({
+/*
+ * @LastEditTime: 2023-03-14 11:46:38
+ * @Descripttion: 
+ */
+module.exports={
   transpileDependencies: true,
   lintOnSave: false, //关闭ESlint校验
   chainWebpack: (config) => {
@@ -8,4 +11,15 @@ export default defineConfig({
       return args;
     });
   },
-});
+}
+// import { defineConfig } from '@vue/cli-service';
+// export default defineConfig({
+//   transpileDependencies: true,
+//   lintOnSave: false, //关闭ESlint校验
+//   chainWebpack: (config) => {
+//     config.plugin('html').tap((args) => {
+//       args[0].title = '你的标题';
+//       return args;
+//     });
+//   },
+// });
