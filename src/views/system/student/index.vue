e161d9268a1696019adced2b3b6d5aa127394f3d
luoguoshun <394024317@qq.com>
2023-03-30 15:53:06 +0800

    new

---
+++ src/views/system/student/index.vue
@@ -0,0 +1,19 @@
+<!--
+ * @LastEditTime: 2023-03-30 15:52:59
+ * @Descripttion: 
+-->
+<template>
+  <div id="student">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
