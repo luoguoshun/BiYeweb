8bb692655a083898cb25ee53a4904adc76cf1ede
luoguoshun <394024317@qq.com>
2023-03-31 11:20:36 +0800

    BB

---
+++ src/views/wbaseInfo/dormitoryRoom/inde.vue
@@ -0,0 +1,20 @@
+<!--
+ * @LastEditTime: 2023-03-31 11:12:47
+ * @Descripttion: 
+-->
+<template>
+  <div id="dormitoryRoom">
+    dormitoryRoom
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
